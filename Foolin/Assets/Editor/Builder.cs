ae9df3895138b4bcd5e0b3ab9a864695222f0386
DeanBrannen <dean@exex.ai>
2024-08-06 14:55:49 -0400

    Switching to TeamCity for CI/CD rollouts

---
+++ Foolin/Assets/Editor/Builder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+	[MenuItem("Build/Build WindowsOS")]
+	public static void PerformWindowsOSBuild()
+	{
+		BuildPlayerOptions bpo = new BuildPlayerOptions();
+		bpo.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+		bpo.locationPathName = "C:/Users/DeanB/Documents/GitHub/KubesTest/TESTBUILDs";
+		bpo.target = BuildTarget.StandaloneWindows64;
+		bpo.options = BuildOptions.None;
+
+		BuildReport report = BuildPipeline.BuildPlayer(bpo);
+		BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log(message: "Build Succes" + summary.totalSize + " bytes");
+        }
+        if (summary.result == BuildResult.Failed)
+		{
+			Debug.Log(message: "Build Succes");
+		}
+
+
+
+    }
+}
